1301b4d6cfe561286266466f5d1aa859c7b37b22
Selman <selman@Selman-Mac.local>
2019-04-25 23:25:27 +0300

    Initial Commit

---
+++ Caught Kenny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Caught Kenny
+//
+//  Created by Selman on 25.04.2019.
+//  Copyright © 2019 Selman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
